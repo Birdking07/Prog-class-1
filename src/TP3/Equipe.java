976dd82504801bdad98cd7e6031f9437cf580d97
Birdking07 <ninjabirdpepito@gmail.com>
2021-12-11 11:35:24 -0500

    Thursday's work final push
    To do list :
    add a header , seperate lines and colums adequately and validate the result

--- src/TP3/Equipe.java
+++ src/TP3/Equipe.java
@@ -1,5 +1,6 @@
 package TP3;
 
+import java.awt.desktop.AppForegroundListener;
 import java.io.File;
 
 import java.util.Scanner;
@@ -13,15 +14,52 @@ public class Equipe {
     private static int contre;
     private static String nomEquipe = "";
     private static int nombreMatch;
-    private static int skillDiff;
+    private static int Diff;
     private static int moy;
     private static int total;
     private static int[] valeursMoyenne;
     private static int[] valeursPoints;
+
+
     public Equipe(int[] valeursPoints, int[] valeursMoyenne, int espacement, String info) {
         this.valeursPoints = valeursPoints;
         this.valeursMoyenne = valeursMoyenne;
         Match_set(info);
+        int getDifferance = Calcul_diff(Diff , pour , contre);
+        int numeroMatch = Calcul_match(nombreMatch , victoires , defaites , egalite);
+        int moyenne = Appel_moy(victoires , egalite , defaites , valeursMoyenne , moy ,numeroMatch);
+        int gettotal = Calcul_total(valeursPoints , victoires, egalite, defaites );
+
+
+
+
+
+        System.out.println(nomEquipe + " " + numeroMatch + " " + victoires + " " + defaites + " " + egalite + " " + pour + " " + contre + " " + getDifferance + " " + moyenne + " " + gettotal);
+    }
+
+    private static int Calcul_match(int nombreMatch , int victoires ,int defaites , int egalite){
+        nombreMatch = victoires + defaites + egalite; //Le nombre de matchs
+        return nombreMatch;
+    }
+
+    private static int Calcul_diff(int Diff , int pour , int contre){
+        Diff = pour - contre; //La différence de Pour et Contre (skill issue)
+        return Diff;
+    }
+
+    private static int Appel_moy(int victoires,int egalite, int defaites, int[] valeursMoyenne , int moy , int numeroMatch){
+        moy = calculMoyenne(victoires, egalite, defaites, valeursMoyenne) / numeroMatch; //Calcul de la moyenne (voir méthode)
+        return moy;
+    }
+
+    //Cette méthode retourne une moyenne pondérée des trois premiers paramètres par rapport aux règles imposées par le quatrième
+    private static int calculMoyenne(int victores, int egalite, int defaites, int[] valeurs){
+        return victoires * valeurs[0] + defaites * valeurs[1] + egalite * valeurs[2];
+    }
+
+    private static int Calcul_total(int[] valeursPoints , int victoires,int egalite,int defaites ){
+        total = calculMoyenne(victoires, egalite, defaites, valeursPoints); //Calcul des points (voir méthode)
+        return total;
     }
 
     private static void Match_set(String info){
@@ -31,16 +69,8 @@ public class Equipe {
         victoires = Integer.parseInt(infos[1]); //Victoires
         defaites = Integer.parseInt(infos[2]); //Défaites
         egalite = Integer.parseInt(infos[3]); //Égalités
-        nombreMatch = victoires + defaites + egalite; //Le nombre de matchs
         pour = Integer.parseInt(infos[4]); //Pour
         contre = Integer.parseInt(infos[5]); //Contre
-        skillDiff = pour - contre; //La différence de Pour et Contre (skill issue)
-        moy = calculMoyenne(victoires, egalite, defaites, valeursMoyenne) / nombreMatch; //Calcul de la moyenne (voir méthode)
-        total = calculMoyenne(victoires, egalite, defaites, valeursPoints); //Calcul des points (voir méthode)
-        System.out.println(nomEquipe + " " + nombreMatch + " " + victoires + " " + defaites + " " + egalite + " " + pour + " " + contre + " " + skillDiff + " " + moy + " " + total);
-    }
-    //Cette méthode retourne une moyenne pondérée des trois premiers paramètres par rapport aux règles imposées par le quatrième
-    private static int calculMoyenne(int victores, int egalite, int defaites, int[] valeurs){
-        return victoires * valeurs[0] + defaites * valeurs[1] + egalite * valeurs[2];
+
     }
 }
